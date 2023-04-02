4c0face70e0ae73c4bffb906ee137d97495b17e8
AlimBodzhgua <bodzhgua2015@mail.ru>
2023-04-02 18:07:12 +0300

    Change table resize structure

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent';
-import {$} from '@core/dom';
 import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.resize';
 
 export class Table extends ExcelComponent {
 	static className = 'table';
@@ -17,57 +17,7 @@ export class Table extends ExcelComponent {
 
 	onMousedown(event) {
 		if (event.target.dataset.resize) {
-			const $resizer = $(event.target);
-			const $parent = $resizer.closest('[data-type="resizable"]');	
-			const coords = $parent.getCoords();
-			const type = $resizer.data.resize;
-
-			const tableHeight = this.$root.getCoords().height;
-			const tableWidth = this.$root.getCoords().width;
-			let value;
-
-			document.onmousemove = (e) => {
-				if (type === 'column') {
-					const delta = Math.floor(e.pageX - coords.right);
-					value = coords.width + delta;
-					$resizer.css({
-						right: -delta + 'px',
-						height: tableHeight + 'px',
-						opacity: 1,
-					})
-				} else {
-					const delta = Math.floor(e.pageY - coords.bottom);
-					value = coords.height + delta;
-					
-					$resizer.css({
-						bottom: -delta + 'px',
-						width: tableWidth + 'px',
-						opacity: 1,
-					})
-				}
-			}
-				
-			document.onmouseup = () => {
-				document.onmousemove = null;
-				if (type === 'column') {
-					$resizer.css({
-						right: 0,
-						height: 100 + '%',
-						opacity: 0,
-					})
-					$parent.css({width: value + 'px'});
-
-					this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-						.forEach(cell => $(cell).css({width: value + 'px'}))
-				} else {
-					$resizer.css({
-						bottom: 0,
-						width: 100 + '%',
-						opacity: 0,
-					})
-					$parent.css({height: value + 'px'});
-				}
-			}
+			resizeHandler(this.$root, event);
 		}
 	}
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,55 @@
+import {$} from '@core/dom';
+
+export function resizeHandler($root, event) {
+	const $resizer = $(event.target);
+	const $parent = $resizer.closest('[data-type="resizable"]');
+	const coords = $parent.getCoords();
+	const type = $resizer.data.resize;
+
+	const tableHeight = $root.getCoords().height;
+	const tableWidth = $root.getCoords().width;
+	let value;
+
+	document.onmousemove = (e) => {
+		if (type === 'column') {
+			const delta = Math.floor(e.pageX - coords.right);
+			value = coords.width + delta;
+			$resizer.css({
+				right: -delta + 'px',
+				height: tableHeight + 'px',
+				opacity: 1,
+			});
+		} else {
+			const delta = Math.floor(e.pageY - coords.bottom);
+			value = coords.height + delta;
+
+			$resizer.css({
+				bottom: -delta + 'px',
+				width: tableWidth + 'px',
+				opacity: 1,
+			});
+		}
+	};
+
+	document.onmouseup = () => {
+		document.onmousemove = null;
+		if (type === 'column') {
+			$resizer.css({
+				right: 0,
+				height: 100 + '%',
+				opacity: 0,
+			});
+			$parent.css({width: value + 'px'});
+
+			$root.findAll(`[data-col="${$parent.data.col}"]`)
+				.forEach((cell) => $(cell).css({width: value + 'px'}));
+		} else {
+			$resizer.css({
+				bottom: 0,
+				width: 100 + '%',
+				opacity: 0,
+			});
+			$parent.css({height: value + 'px'});
+		}
+	};
+}
